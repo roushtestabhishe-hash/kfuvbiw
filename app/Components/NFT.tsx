cfdf6c8e27765c01c4b688bc753cb7d5b3f95cfd
LAMDA <58250902+dianasuar@users.noreply.github.com>
2025-09-16 11:14:41 +0530

    Update NFT.tsx

--- app/Components/NFT.tsx
+++ app/Components/NFT.tsx
@@ -10,7 +10,7 @@ import BuyNowButton from "./BuyNowButton";
 // 🔌 wagmi for on-chain call
 import { useAccount, useReadContract, useWriteContract } from "wagmi";
 // your minimal ABI file you created in app/abi/nftClaim.ts
-import { nftClaimAbi } from "@/app/abi/nftClaim";
+import { nftClaimAbi } from "../app/abi/nftClaim";
 
 interface NFTProps {
   onBack: () => void;
