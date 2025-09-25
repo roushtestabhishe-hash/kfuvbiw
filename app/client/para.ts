84d8c46e282915709fe342e08a7d9c673581d9fd
LAMDA <58250902+dianasuar@users.noreply.github.com>
2025-09-26 01:25:33 +0530

    Update para.ts

--- app/client/para.ts
+++ app/client/para.ts
@@ -1,10 +1,10 @@
-//'use client'
-//import { Environment, ParaWeb } from "@getpara/react-sdk";
+'use client'
+import { Environment, ParaWeb } from "@getpara/react-sdk";
 
-//const API_KEY = "cbaab0dbc16b98e540bb4b6d5322c6f1";
+const API_KEY = "prod_f3850e5a8b23ef654753697159813d91";
 
-//if (!API_KEY) {
- // throw new Error("API key is not defined. Please set NEXT_PUBLIC_PARA_API_KEY in your environment variables.");
-//}
+if (!API_KEY) {
+ throw new Error("API key is not defined. Please set NEXT_PUBLIC_PARA_API_KEY in your environment variables.");
+}
 
-//export const para = new ParaWeb(Environment.PRODUCTION, API_KEY);
+export const para = new ParaWeb(Environment.PRODUCTION, API_KEY);
